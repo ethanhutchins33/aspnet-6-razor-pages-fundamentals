e9ed462ad24fc2b2a1b2740151abcdf0a1ff75be
Ethan Hutchins <ethanhutchins33@gmail.com>
2023-09-29 14:35:56 +0100

    Add the Survey results widget to the dashboard. - Gets the survey data from survey.json file - loop over the survey results and dynamically create the table in the razor page

--- WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
+++ WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
@@ -1,19 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffeeAdmin.Data.Models;
 
-namespace WiredBrainCoffeeAdmin.Pages
+namespace WiredBrainCoffeeAdmin.Pages;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
     {
-        private readonly ILogger<IndexModel> _logger;
+        _logger = logger;
+    }
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+    public List<SurveyItem> SurveyResults { get; set; }
 
-        public void OnGet()
-        {
+    public void OnGet()
+    {
+        var rawJson = System.IO.File
+            .ReadAllText("wwwroot/sampledata/survey.json");
 
-        }
+        SurveyResults = JsonSerializer.Deserialize<List<SurveyItem>>(rawJson);
     }
 }
