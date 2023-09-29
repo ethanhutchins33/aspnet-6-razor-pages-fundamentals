05e45d5a42db0c09be601c144104910bd7b4c181
Ethan Hutchins <ethanhutchins33@gmail.com>
2023-09-29 16:20:35 +0100

    Add a new form to the AddProduct.cshtml razor page

--- WiredBrainCoffeeAdmin/Pages/Products/AddProduct.cshtml.cs
+++ WiredBrainCoffeeAdmin/Pages/Products/AddProduct.cshtml.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffeeAdmin.Data.Models;
 
 namespace WiredBrainCoffeeAdmin.Pages.Products;
 
 public class AddProduct : PageModel
 {
+    public Product? NewProduct { get; set; }
+
     public void OnGet()
     {
     }
