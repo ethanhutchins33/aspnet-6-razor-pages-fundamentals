bf189fef5bdb77a0fd41126121b71b1bb0a66326
Ethan Hutchins <ethanhutchins33@gmail.com>
2023-09-29 11:21:51 +0100

    Add new pages - AddProduct page - ViewAllProducts page

---
+++ WiredBrainCoffeeAdmin/Pages/Products/ViewAllProducts.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WiredBrainCoffeeAdmin.Pages.Products;
+
+public class ViewAllProducts : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
